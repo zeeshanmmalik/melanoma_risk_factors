c6e2d8e5c85d55cff5b1b263af5acf0446b75eb8
Zeeshan Malik <zeeshan.m.malik@gmail.com>
2013-01-02 20:36:38 -0500

    primary changed from id to patient_id

--- db/migrate/20120926164633_create_patients.rb
+++ db/migrate/20120926164633_create_patients.rb
@@ -1,6 +1,6 @@
 class CreatePatients < ActiveRecord::Migration
   def change
-    create_table :patients do |t|
+    create_table :patients, {:primary_key => :patient_id} do |t|
       t.string :mrn
 
       t.timestamps
